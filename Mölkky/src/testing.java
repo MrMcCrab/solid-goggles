19dc041b550ce31424e53922f113b80c6ebcb8df
J Heinonen <juusoheinonen91@gmail.com>
2017-02-28 09:37:11 +0200

    Improved testing.java

--- Mölkky/src/testing.java
+++ Mölkky/src/testing.java
@@ -3,8 +3,11 @@ import java.util.Random;
 public class testing {
 	public static void main(String[] args) {
 		Random rand = new Random();
-		int luku = rand.nextInt(6);
-		System.out.println(luku);
+		
+		for(int i = 1; i < 10; i++){
+			int luku = rand.nextInt(6);
+			System.out.println(luku+1);
+		}
 	
 }
 }

d4f8b78e73928597a98a1d976820b18c25236f7c
J Heinonen <juusoheinonen91@gmail.com>
2017-02-28 09:34:43 +0200

    New class

---
+++ Mölkky/src/testing.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class testing {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int luku = rand.nextInt(6);
+		System.out.println(luku);
+	
+}
+}

8c0db8a7e5e7b75e3f02bd43e52d173c78c5ceaa
MrMcCrab <>
2017-03-14 07:57:27 +0200

    updates

--- Mölkky/src/Player.java
+++ Mölkky/src/Player.java
@@ -1,4 +1,28 @@
 
 public class Player {
 
+	private final int MAX_POINTS = 50;
+	private String name;
+	private int points;
+	
+	Player(String aName){
+		
+		this.name = aName;	
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public int getPoints(){
+		return this.points;
+	}
+	
+	public int setPoints(){
+		this.points += points;
+		if (this.points > MAX_POINTS){
+			this.points = 25;
+		}
+		return this.points;
+	}
 }
